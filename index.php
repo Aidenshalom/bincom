b0879b7202c0d6924bf9cffacbb3e1b150165205
Aidenshalom <157998665+Aidenshalom@users.noreply.github.com>
2026-07-11 13:06:03 +0200

    Add initial index.php with Bootstrap layout

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    require "config/database.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Bincom</title>
+</head>
+<body>
+
+    <div class="container">
+        <h2> Bincom Preliminary Online Interview</h2>
+        <a class="btn btn-primary" href="pages/polling-unit.php" role="button">Check Polling Unit Result</a>
+        <a class="btn btn-primary" href="pages/lga-results.php" role="button">Check Summmed Total Results</a>
+        <a class="btn btn-primary" href="pages/add-result.php" role="button">Add Polling Unit</a>
+
+   
+        
+ 
+    </div>
+    
+</body>
+</html>
